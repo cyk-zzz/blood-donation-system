d3aa585237addcdc7b8aaff46d9be813b58ad8a9
Joshua Lai <87967796+joshualai1314@users.noreply.github.com>
2022-06-19 23:54:06 +0800

    enum details

--- State.java
+++ State.java
@@ -1,3 +1,15 @@
 public enum State {
-    
+    Perlis,
+    Kedah,
+    Penang,
+    Kelantan,
+    Terengganu,
+    Perak,
+    Pahang,
+    Selangor,
+    Negeri_Sembilan,
+    Melaka,
+    Johor,
+    Sabah,
+    Sarawak
 }
