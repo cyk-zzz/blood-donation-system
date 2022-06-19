299a3b43d690d6bd0c9b7c737271ec2e1d0cafcd
Joshua Lai <87967796+joshualai1314@users.noreply.github.com>
2022-06-20 00:17:11 +0800

    1.1

--- BloodDonationEvent.java
+++ BloodDonationEvent.java
@@ -1,3 +1,27 @@
 public class BloodDonationEvent {
+    private int eventID;
+    private State eventState;
+    private String eventAddress;
+    private LocalDate eventDate;
+    private Vector <User> donorlist = new Vector <User>();
+
+    public BloodDonationEvent(int eventID,State evesState,String eventDate,Data date){
+        this.eventID=eventID;
+        this.eventState=evesState;
+        this.eventDate=eventDate;
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+    public State getEventState(){
+        return eventState;
+    }
+    public String getEventAddress(){
+        return eventAddress;
+    }
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
     
 }
